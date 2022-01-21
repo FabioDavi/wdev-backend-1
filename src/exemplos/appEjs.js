be2b8cf717c5c19ecfef5b794d0dde03c6e19f0d
Dev-js <59389855+FabioDavi@users.noreply.github.com>
2022-01-21 17:02:23 -0300

    tirei texto

--- src/exemplos/appEjs.js
+++ src/exemplos/appEjs.js
@@ -4,31 +4,7 @@ const app = express()
 require('dotenv').config()
 
 const port = process.env.PORT || '3080'
-//fazer esse https://github.com/woliveiras/curriculo-expresso
-/*
-If I’m hiring a Fullstack Nodejs developer I’m looking for the following:
 
-Experience with Express concepts (routes, how the server works, req/res, locals…)
-Experience with one of the common ORMs (Sequelize, Sails…)
-MVC for the web (how models, views, controllers are fairly standardized for Nodejs apps)
-Experience with a templating language (Mustache, Handlebars, EJS…)
-Modern JavaScript language features (promises + async/await, ES6 & 7 syntax…)
-Experience with a front end framework (React, Angular, Backbone…)
-Modern CSS 3.0 & HTML
-Some Database experience (MySQL, Mongo, Postgres…)
-For Senior Developers
-
-Performance considerations (parallelization with async, minimizing HTTP requests, minimizing DOM interaction, building CSS / JS front end files)
-Very detailed syntax knowledge (senior developers memorize syntax because they’ve coded in it everyday for years, junior developers might 
-    still need the docs)
-Security Knowledge (CSRF, SQL Injection prevention, JS injection prevention…)
-Server setup & administration (E.g. ins and outs of AWS, DNS setup, DB setup, Docker, etc.)
-When I say “Experience” that means work, freelance, or personal projects. Developers have to change languages all the time as their 
-career progresses, so it doesn’t make sense to always expect someone has coded Nodejs in their current organization, but 
-you shouldn’t be applying to a Nodejs position if you haven’t at least built a side project in Node and have gone through the 
-experience of hello world to basic reading from a database to a simple asynchronous-on-the-backend, single-page-on the-front-end app.
-
-*/
 app.set('view engine', 'ejs');
 app.set('views', './src/exemplos/views');
 
